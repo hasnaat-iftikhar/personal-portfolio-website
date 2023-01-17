d4281727dd0032ae8277aec384e5df18db004fa4
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-01-17 18:05:58 +0500

    Worked on faq section

--- components/sections/faq/index.js
+++ components/sections/faq/index.js
@@ -13,7 +13,6 @@ const FaQSection = () => {
   return (
     <section className={styles.faq_section}>
       <div className={styles.container}>
-        <h2 className={styles.section_title}>Frequently asked questions</h2>
         <div className={styles.all_faqs}>
           {faqs.map((data, index) => (
             <Accordion
