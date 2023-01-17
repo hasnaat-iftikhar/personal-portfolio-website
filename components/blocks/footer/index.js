b34601229b5ea75e543aee5eb09c21e3b8ae46bb
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-01-17 18:15:45 +0500

    Worked on footer

--- components/blocks/footer/index.js
+++ components/blocks/footer/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Components
+import Button from "../../elements/button";
+
 // Styles
 import styles from "./styles.module.scss";
 
@@ -7,9 +10,12 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h4 className={styles.contactInfo}>
-          Contact Info: <span>+92 301 4717189</span>
-        </h4>
+        <h3 className={styles.sectionTitle}>Let's meet!</h3>
+        <p className={styles.subTitle}>
+          Book a free 15-minute session where we will discuss your project, your
+          issues and how to solve them.
+        </p>
+        <Button>Let's build a project together!</Button>
       </div>
     </footer>
   );
