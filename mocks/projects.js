f100c9b17bf145cbc01090ac31d554ee343b1070
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-02-12 17:38:14 +0500

    Code refactoring

--- mocks/projects.js
+++ mocks/projects.js
@@ -27,7 +27,7 @@ export const latestProjects = [
     image: "/images/projects/thumbnail-size.png",
     url: "/",
     mini_description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
   },
   {
     name: "Project 2",
